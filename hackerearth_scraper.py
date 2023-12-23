141b7acbb3898739cd583325f914929e4e9c2d34
PriyanshiAgrawal1345 <128245760+PriyanshiAgrawal1345@users.noreply.github.com>
2023-12-24 01:01:17 +0530

    Add files via upload

---
+++ hackerearth_scraper.py
@@ -0,0 +1,65 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import json 
+import os
+
+def scrape_hearth(url):
+    options = Options()
+    options.add_argument('--headless')
+
+    browser = webdriver.Firefox(options=options)
+
+    browser.get(url)
+    browser.implicitly_wait(10)
+
+    login_element = browser.find_element_by_link_text('Log in')
+    login_element.click()
+    usrname = browser.find_element_by_id("id_login")
+    pwd = browser.find_element_by_id("id_password")
+
+    usrname.send_keys(os.environ.get("username"))
+    pwd.send_keys(os.environ.get("passwd"))
+    submit_btn = browser.find_element_by_name("submit")
+    pwd.send_keys(u'\ue007')
+
+    followers_element = browser.find_element_by_link_text('Followers')
+    followers_element.click()
+
+    # # now on the followers page
+    data = {}
+
+    # # scroll to get all followers in visible frame
+    # view_more_btn = browser.find_element_by_xpath("//a[contains(@class, 'load-scroll-content button btn-blue hidden')]")
+    # scroll_end = browser.find_element_by_xpath("//p[contains(@class, 'no-scroll-content-message body-font gray-text hidden')]")
+
+    # while not scroll_end.is_displayed():
+    #     try:
+    #         view_more_btn.click()
+    #     except Exception as e:
+    #         pass
+
+    # scrape followers
+    all_followers = browser.find_elements_by_xpath("//a[contains(@class, 'track-follower-user regular dark weight-600')]")
+
+    for i, follower in enumerate(all_followers):
+        data[i] = {}
+        data[i]["name"] = follower.text
+        data[i]["profile_link"] = follower.get_attribute("href")
+        
+    for i in data:
+        try:
+            browser.get(data[i]["profile_link"])
+            skills = browser.find_element_by_xpath("//div[contains(@class, 'attributes regular dark')]")
+            data[i]["skills"] = skills.text
+            data[i]["skills"] = [sk.strip() for sk in data[i]["skills"].split(',')]
+            current_job = browser.find_element_by_xpath("//span[contains(@class, 'text')]")
+            data[i]["cuurent_job"] = current_job.text
+            browser.implicitly_wait(2)
+        
+        except Exception as e:
+            pass
+
+    with open("scrapeddata.json", 'w') as f:
+        json.dump(data, f)
+
+    print("Hackerearth scraping successful")
---
+++ main.py
@@ -0,0 +1,70 @@
+from os import error
+from flask import Flask, flash, render_template, request
+from hackerearth_scraper import *
+
+mapquery = {
+    "webd": ['html', 'flask', 'css', 'bootstrap', 'express', 'script', 'javascript', 'rest', 'django', 'node', 'sql', 'react', 'deploy', 'php', 'server', 'ruby'],
+    "sde": ['c++', 'cpp', 'c', 'java', 'script', 'python'],
+    "mlai": ['jupyter', 'r', 'machine', 'learn', 'scikit', 'tensor', 'pytorch', 'data', 'cuda'],
+    "javad": ['java']
+}
+def read_data():
+    with open('scrapeddata.json', 'r') as f:
+        return json.load(f)
+
+
+def score_queries(data, param):
+    scores={} # id, score
+    for x in data:
+        if 'skills' in data[x]:
+            for sk in data[x]['skills']:
+                for keyw in param:
+                        if keyw in sk.lower():
+                            try:
+                                scores[x] += 1
+                            except KeyError:
+                                scores[x] = 1
+
+    scores = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+    ranked_data = {}
+    for x in scores:
+        ranked_data[x[0]] = data[x[0]]
+    return ranked_data
+
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+@app.route("/", methods=['GET'])
+def index():
+    return render_template("index.html")
+
+@app.route("/result", methods=['POST'])
+def result():
+    recruiter_url = request.form['recruiter']
+
+    if "hackerearth" in recruiter_url:
+        scrape_hearth(recruiter_url)
+        data = read_data()
+        return render_template("out.html", profiles=data)
+
+    error = "Please provide a valid HackerEarth profile link"
+    flash(error)
+    return render_template("index.html", error=error)
+
+
+@app.route("/query", methods=['POST'])
+def query():
+    query_req = request.form['sortby']
+
+    if query_req == 'default':
+        return render_template("out.html", profiles=read_data())
+
+    query_data = score_queries(read_data(), mapquery[request.form['sortby']])
+    return render_template("out.html", profiles=query_data, sel=query_req)
+
+if __name__ == "__main__":
+    # app.secret_key = 'supersecretkey'
+    # app.config['SESSION_TYPE'] = 'filesystem'
+    #sess.init_app(app)
+    app.run(host="127.0.0.1", port=8080, debug=True)
